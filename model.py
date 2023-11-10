d9bfbb023ae44c1654b90aef71ea9ec002c02424
Jannes Elstner <janneselstner@gmail.com>
2023-11-10 13:20:39 +0100

    Initial commit

---
+++ model.py
@@ -0,0 +1,187 @@
+import math
+
+import einops
+import torch
+from jaxtyping import Float, Int
+from torch import nn, Tensor
+from einops import einsum
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model, norm_eps):
+        super().__init__()
+        self.norm_eps = norm_eps
+        self.g = nn.Parameter(torch.ones(d_model))
+        self.b = nn.Parameter(torch.zeros(d_model))
+
+    def forward(
+            self,
+            residual: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        residual_std = (torch.var(residual, dim=-1, keepdim=True, unbiased=False) + self.norm_eps).sqrt()
+        normalized = residual / residual_std
+        out = normalized * self.g + self.b
+        return out
+
+
+class Embed(nn.Module):
+    def __init__(self, d_vocab, d_model):
+        super().__init__()
+        self.W_E = nn.Parameter(torch.empty((d_vocab, d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+
+    def forward(
+            self,
+            tokens: Int[Tensor, "batch posn"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        return self.W_E[tokens]
+
+
+class Unembed(nn.Module):
+    def __init__(self, d_model, d_vocab, init_range):
+        super().__init__()
+        self.W_U = nn.Parameter(torch.empty((d_model, d_vocab)))
+        nn.init.normal_(self.W_U, std=init_range)
+        self.b_U = nn.Parameter(torch.zeros(d_vocab, requires_grad=False))
+
+    def forward(
+            self,
+            normalized_resid_final: Float[Tensor, "batch position d_model"]
+    ) -> Float[Tensor, "batch position d_vocab"]:
+        return einsum(normalized_resid_final, self.W_U, "batch posn d_model, d_model d_vocab -> batch posn d_vocab") + self.b_U
+
+
+class RotaryPositionalEmbeddings(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.W_pos = nn.Parameter(torch.empty((cfg.max_posn, cfg.d_model)))
+        nn.init.normal_(self.W_pos, std=self.init_range)
+
+    def forward(self, posns):
+        # positions: [batch, posn]
+        embeddings = self.W_pos[posns]
+        return embeddings
+
+
+class GroupedMultiQueryAttention(nn.Module):
+    IGNORE: Float[Tensor, ""]
+
+    def __init__(self, d_model, n_q_heads, n_kv_heads, d_head, init_range, device):
+        super().__init__()
+        self.n_kv_heads = n_kv_heads
+        self.d_head = d_head
+        self.W_Q = nn.Parameter(torch.empty((n_q_heads, d_model, d_head)))
+        self.W_K = nn.Parameter(torch.empty((n_kv_heads, d_model, d_head)))
+        self.W_V = nn.Parameter(torch.empty((n_kv_heads, d_model, d_head)))
+        self.W_O = nn.Parameter(torch.empty((n_q_heads, d_head, d_model)))
+        nn.init.normal_(self.W_Q, std=init_range)
+        nn.init.normal_(self.W_K, std=init_range)
+        nn.init.normal_(self.W_V, std=init_range)
+        nn.init.normal_(self.W_O, std=init_range)
+        self.register_buffer("IGNORE", torch.tensor(-1e5, dtype=torch.float32, device=device))
+
+    def forward(
+            self,
+            normalized_resid_pre: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        # Calculate query, key and value vectors
+        q = einsum(normalized_resid_pre, self.W_Q,
+                   "batch posn d_model, n_q_heads d_model d_head -> batch posn n_q_heads d_head")
+        k = einsum(normalized_resid_pre, self.W_K,
+                   "batch posn d_model, n_kv_heads d_model d_head -> batch posn n_kv_heads d_head")
+        v = einsum(normalized_resid_pre, self.W_V,
+                   "batch posn d_model, n_kv_heads d_model d_head -> batch posn n_kv_heads d_head")
+
+        # Calculate attention scores, then scale and mask, and apply softmax to get probabilities
+        grouped_q = einops.rearrange(
+            q, "batch posn n_q_heads d_head -> batch posn (group_size, n_kv_heads) d_head", n_kv_heads=self.n_kv_heads
+        )
+        attn_scores = einsum(grouped_q, k,
+                             "batch posn_q g_size n_kv_heads d_head, batch posn_k n_kv_heads d_head -> batch g_size n_kv_heads posn_q posn_k")
+        attn_scores_masked = self.apply_causal_mask(attn_scores / math.sqrt(self.d_head))
+        attn_pattern = attn_scores_masked.softmax(-1)
+
+        # Take weighted sum of value vectors, according to attention probabilities
+        z = einsum(v, attn_pattern,
+                   "batch posn_k n_kv_heads d_head, batch g_size n_kv_heads posn_q posn_k -> batch posn_q g_size n_kv_heads n_head")
+
+        # Calculate output (by applying matrix W_O and summing over heads, then adding bias b_O)
+        z = einops.rearrange(z, "batch posn_q g_size n_kv_heads d_head -> batch posn_q (g_size n_kv_heads) d_head")
+        attn_out = einsum(z, self.W_O, "batch posn_q n_q_heads d_head, n_q_heads d_head d_model -> batch posn_q d_model")
+        return attn_out
+
+    def apply_causal_mask(
+            self,
+            attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"]
+    ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
+        '''
+        Applies a causal mask to attention scores, and returns masked scores.
+        '''
+        # Define a mask that is True for all positions we want to set probabilities to zero for
+        all_ones = torch.ones(attn_scores.shape[2:], device=attn_scores.device)
+        mask = torch.triu(all_ones, diagonal=1).bool()
+        # Apply the mask to attention scores, then return the masked scores
+        attn_scores.masked_fill_(mask, self.IGNORE)
+        return attn_scores
+
+
+class FeedForwardSwiGLU(nn.Module):
+    def __init__(self, d_model, d_mlp, init_range, device):
+        super().__init__()
+        self.W_in = nn.Parameter(torch.empty((d_model, d_mlp)))
+        self.W_out = nn.Parameter(torch.empty((d_mlp, d_model)))
+        self.b_in = nn.Parameter(torch.zeros((d_mlp)))
+        self.b_out = nn.Parameter(torch.zeros((d_model)))
+        nn.init.normal_(self.W_in, std=init_range)
+        nn.init.normal_(self.W_out, std=init_range)
+
+    def forward(
+            self,
+            normalized_resid_mid: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        pre = einsum(normalized_resid_mid, self.W_in, "batch position d_model, d_model d_mlp -> batch position d_mlp") + self.b_in
+        post = nn.functional.relu(pre)  # TODO: SwiGLU
+        mlp_out = einsum(post, self.W_out, "batch position d_mlp, d_mlp d_model -> batch position d_model") + self.b_out
+        return mlp_out
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_model, n_heads, d_head, d_mlp, norm_eps, init_range, device):
+        super().__init__()
+        self.norm1 = RMSNorm(d_model, norm_eps)
+        self.attention = GroupedMultiQueryAttention(d_model, n_heads, d_head, init_range, device)
+        self.norm2 = RMSNorm(d_model, norm_eps)
+        self.feed_forward = FeedForwardSwiGLU(d_model, d_mlp, init_range, device)
+
+    def forward(
+            self,
+            resid_pre: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        resid_mid = self.attention(self.norm1(resid_pre)) + resid_pre
+        resid_post = self.feed_forward(self.norm2(resid_mid)) + resid_mid
+        return resid_post
+
+
+class LLamaTransformer(nn.Module):
+    def __init__(self, d_vocab, d_model, n_heads, d_head, d_mlp, n_layers, norm_eps, init_range, device):
+        super().__init__()
+        self.embed = Embed(d_vocab, d_model)
+        self.blocks = nn.ModuleList([
+            TransformerBlock(d_model, n_heads, d_head, d_mlp, norm_eps, init_range, device)
+            for _ in range(n_layers)
+        ])
+        self.norm_final = RMSNorm(d_model, norm_eps)
+        self.unembed = Unembed(d_model, d_vocab, init_range)
+
+    def forward(
+            self,
+            tokens: Int[Tensor, "batch posn"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        # Calculate embeddings for tokens and positions
+        token_embeddings = self.embed(tokens)
+
+        residual = token_embeddings
+        for block in self.blocks:
+            residual = block(residual)
+        logits = self.unembed(self.norm_final(residual))
+        return logits
